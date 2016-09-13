2817e7f81ab71419d24c94dd922c3004f933ab26
lazizgueye <lazizgueye@gmail.com>
2016-09-13 20:48:09 +0200

    siteweb V1.1

---
+++ client/app/configRoute.js
@@ -0,0 +1,52 @@
+
+//configRoute.js
+'use strict';
+
+/**
+ * Déclaration de l'application routeApp
+ */
+var routeApp = angular.module('routeApp', [
+    // Dépendances du "module"
+    'ngRoute',
+    'routeAppControllers'
+]);
+
+/**
+ * Configuration du module principal : routeApp
+ */
+routeApp.config(['$routeProvider',
+    function($routeProvider) { 
+        
+        // Système de routage
+        $routeProvider
+        .when('/home', {
+            templateUrl: 'views/home.html',
+            controller: 'homeCtrl'
+        })
+        .when('/search/:query', {
+            templateUrl: 'views/search.html',
+            controller: 'searchCtrl'
+        })
+		.when('/detail/:id', {
+            templateUrl: 'views/detail.html',
+            controller: 'detailCtrl'
+        })
+	
+		.when('/achat', {
+            templateUrl: 'views/achat.html',
+            controller: 'achatCtrl'
+        })
+
+
+	.otherwise({
+            redirectTo: '/home'
+        });
+	
+    }
+]);
+
+
+/**
+ * Définition des contrôleurs
+ */
+var routeAppControllers = angular.module('routeAppControllers', []);
