1232c93b8f6520baf8e5deb615c6bd7902cced00
Jeremy <leyvraz.jrm@gmail.com>
2016-09-22 11:02:07 +0200

    bug: trailer problem

--- src/main/webapp/client/app/scripts/services/services.js
+++ src/main/webapp/client/app/scripts/services/services.js
@@ -9,17 +9,12 @@ routeAppControllers.factory('servicesSearch',['$http' ,function ($http) {
 		search:function(){
 		  return $http.get('client/app/json/jsonListDeFilm.php');
 		},
-<<<<<<< HEAD
 		searchFilm:function(chaine){  
 			return	$http.get('http://localhost:8080/e-movies/rest/videos/search/all/movie/'+chaine);
-=======
-                searchFilm:function(chaine){  
-			return	$http.get('http://localhost:8080/e-movies/rest/vidéos/search/all/movie/'+chaine);
->>>>>>> development
 			//return	$http.get('client/app/json/jsonListDeFilm.php');     
 		},
 		detailsFilm:function(id){  
-			return	$http.get('http://localhost:8080/e-movies/rest/vidéos/search/exact/movie/'+id);
+			return	$http.get('http://localhost:8080/e-movies/rest/videos/search/exact/movie/'+id);
 			//return	$http.get('client/app/json/jsonListDeFilm.php');     
 		},		
 		searchSerie:function(chaine){      
