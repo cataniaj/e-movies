662c7127e636387c5f5ad31c19ba7531565c7bf5
Jeremy <leyvraz.jrm@gmail.com>
2016-09-23 16:03:56 +0200

    remove cart.java

--- src/main/java/fr/imag/ejb/dbaccess/CartDatabaseAccessEJB.java
+++ src/main/java/fr/imag/ejb/dbaccess/CartDatabaseAccessEJB.java
@@ -5,6 +5,23 @@
 
 package fr.imag.ejb.dbaccess;
 
+import javax.ejb.ConcurrencyManagement;
+import javax.ejb.ConcurrencyManagementType;
+import javax.ejb.Local;
+import javax.ejb.Singleton;
+import javax.inject.Inject;
+import javax.persistence.EntityManager;
+
+@Singleton
+@Local
+@ConcurrencyManagement(ConcurrencyManagementType.BEAN)
 public class CartDatabaseAccessEJB {
 
+	@Inject EntityManager em;
+	
+	
+	
+	
+	
+	
 }
