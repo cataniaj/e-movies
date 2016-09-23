c8f7f0ad413769091a4ede6be125140d28f9f369
Jeremy <leyvraz.jrm@gmail.com>
2016-09-23 12:06:33 +0200

    Revert "add cart class"
    This reverts commit 4e2b308553c16ccd603e670866f8e941ad8efe55.

--- src/main/java/fr/imag/session/Cart.java
+++ src/main/java/fr/imag/session/Cart.java
@@ -13,48 +13,39 @@ import javax.annotation.PreDestroy;
 import javax.ejb.Stateful;
 import javax.json.JsonObject;
 
-import fr.imag.entities.User;
-
 @Stateful
 public class Cart implements CartLocal{
 	
 	private List<Item> allItemInTheCart;
-	private User customer;
 	
 	@PostConstruct
 	public void initialize(){
 		allItemInTheCart = new ArrayList<Item>();
 	}
-
-	@Override
-	public boolean addItem(Item item) {
-		try{
-			for(Item i : allItemInTheCart){
-				if(i.getIdProduct() == item.getIdProduct()){
-					i.setQuantity(i.getQuantity()+1);
-					return true;
-				}
+	
+	private boolean findItemInTheCart(Item item){
+		for(Item i : allItemInTheCart){
+			if(i.getIdProduct() == item.getIdProduct()){
+				return true;
 			}
-			allItemInTheCart.add(item);
-			return true;
-		}catch(Exception e){
-			return false;
 		}
+		return false;
 	}
 
 	@Override
-	public boolean removeItem(Item item) {
-		try{
-			for(Item i : allItemInTheCart){
-				if(i.getIdProduct() == item.getIdProduct()){
-					allItemInTheCart.remove(i);
-					return true;
-				}
+	public void addItem(Item item) {
+		for(Item i : allItemInTheCart){
+			if(i.getIdProduct() == item.getIdProduct()){
+				i.setQuantity(i.getQuantity()+1);
+				return ;
 			}
-			return false;
-		}catch(Exception e){
-			return false;
 		}
+		allItemInTheCart.add(item);
+	}
+
+	@Override
+	public void removeItem(Item item) {
+		
 	}
 
 	@Override
@@ -63,72 +54,13 @@ public class Cart implements CartLocal{
 	}
 
 	@Override
-	public boolean remove() {
-		try{
-			allItemInTheCart.clear();
-			return true;
-		}catch(Exception e){
-			return false;
-		}
+	public void remove() {
+		allItemInTheCart.clear();
 	}
 	
 	@PreDestroy
-	public boolean delete(){
-		try{
-			allItemInTheCart = null;
-			return true;
-		}catch(Exception e){
-			return false;
-		}
+	public void delete(){
+		allItemInTheCart = null;
 	}
-
-	@Override
-	public boolean addOneCopy(Item item) {
-		try{
-			for(Item i : allItemInTheCart){
-				if(i.getIdProduct() == item.getIdProduct()){
-					i.setQuantity(i.getQuantity()+1);
-					return true;
-				}
-			}
-			return false;
-		}catch(Exception e){
-			return false;
-		}
-	}
-
-	@Override
-	public boolean removeOneCopy(Item item) {
-		try{
-			for(Item i : allItemInTheCart){
-				if(i.getIdProduct() == item.getIdProduct()){
-					allItemInTheCart.remove(i);
-					return true;
-				}
-			}
-			return false;
-		}catch(Exception e){
-			return false;
-		}
-	}
-	
-	@Override
-    public boolean validate(){
-		// TODO creation order, orderLine
-    	return true;
-    }
-	
-	private void updateStockWhenRemoveOneItem(){
-		// TODO creation order, orderLine
-	}
-	
-	private void updateStockWhenAddOneItem(){
-		// TODO update stock + 1
-	}
-	
-	private void updateStockWhenRemoveCart(){
-		// TODO update stock - 1
-	}
-	
 	
 }
--- src/main/java/fr/imag/session/CartLocal.java
+++ src/main/java/fr/imag/session/CartLocal.java
@@ -10,16 +10,8 @@ import javax.json.JsonObject;
 
 @Remote
 public interface CartLocal {
-    
-	public boolean addItem(Item item);
-    public boolean addOneCopy(Item item);
-    
-    public boolean removeItem(Item item);
-    public boolean removeOneCopy(Item item);
-    
+    public void addItem(Item item);
+    public void removeItem(Item item);
     public JsonObject getAllItem();
-    public boolean remove();
-    
-    public boolean validate();
-    
+    public void remove();
 }
