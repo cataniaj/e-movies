86ead1e0b2ead2a5ad486fe1336f94399b8a7bab
Jeremy <leyvraz.jrm@gmail.com>
2016-09-23 14:34:32 +0200

    add cart files
    Cart.java
    CartLocal
    Item
    ItemCart

--- src/main/java/fr/imag/session/CartLocal.java
+++ src/main/java/fr/imag/session/CartLocal.java
@@ -5,6 +5,19 @@
 
 package fr.imag.session;
 
+import javax.json.JsonObject;
+
 public interface CartLocal {
+
+public boolean addItem(Item item);
+public boolean addOneCopy(Item item);
+
+public boolean removeItem(Item item);
+public boolean removeOneCopy(Item item);
+
+public JsonObject getAllItem();
+public boolean remove();
+
+public boolean validate();
 
 }
